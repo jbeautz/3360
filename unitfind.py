0b9f372435b98ae28d33bd67e579f1da2ac907bb
jbeautz <jack@beautz.net>
2020-04-27 18:53:15 -0400

    NA

--- unitfind.py
+++ unitfind.py
@@ -1,10 +1,2 @@
-x = 301
-Need = False
-while (Need==False & x <1000):
-    x = x+1
-    Need = ((x-1)%2==0)&((x-1)%3==0)&((x-1)%4==0)&((x-1)%5==0)&((x-1)%6==0)&(x%7==0)
-    print(Need)
-    print(x)
-
-
-print(x)
+x = ((2604)**85)
+print(x%7081)
